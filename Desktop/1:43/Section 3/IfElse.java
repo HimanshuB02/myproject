31bb6ef77c44d883cde242407ecd4dbc4b7ebd8b
HimanshuB02 <Himanshubxt02@gmail.com>
2022-07-16 20:40:31 +0530

    promises_on_js

---
+++ Desktop/1:43/Section 3/IfElse.java
@@ -0,0 +1,16 @@
+public class IfElse {
+ 
+    public static void main(String[] args) {
+        int grade = 54;
+       if (grade >=90){
+           System.out.println("Congratulation !! you got A");
+        }else if (grade>=80){
+    
+           System.out.println("Good you got B");
+       }else if (grade>=70){
+           System.out.println("You got C");
+       }else{
+           System.out.println("You got nothing!! lol");
+       }
+    }
+}
